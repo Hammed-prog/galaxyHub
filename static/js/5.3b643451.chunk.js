18fa4082dfcab13f3fef8bb39ed5e683f0a2b456
uzma <92790017+uzma-nazim@users.noreply.github.com>
2023-02-18 23:14:44 +0500

    done

---
+++ static/js/5.3b643451.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[5],{812:function(p,n){},826:function(p,n){}}]);
+//# sourceMappingURL=5.3b643451.chunk.js.map
---
+++ static/js/7.dfe6d980.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[7],{779:function(p,s){}}]);
+//# sourceMappingURL=7.dfe6d980.chunk.js.map
